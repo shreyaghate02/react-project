f737418c98d6d98d01ede42597cedfb952c48e41
Shreya Ghate <shreya.ghate.sg@gmail.com>
2019-12-09 23:41:52 +0530

    Single Page Applications Part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,9 @@
 import React from 'react';
 // import moment from 'moment';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody,
+    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
    
     function RenderComments({comments}) {
         // if(comments != null){
@@ -44,16 +47,28 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
         if (props.dish != null) {
             return (
                 <div className="container">
-                    <div className="row">
-                        <div  className="col-12 col-md-5 m-1">
+                <div className="row">
+                    <Breadcrumb>
+
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
                         <RenderDish dish={props.dish} />
-                        </div>
-                        <div  className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.dish.comments} />
-                        </div> 
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComments comments={props.comments} />
                     </div>
                 </div>
+                </div>
             );
+            
         } else {
             return (
                 <div></div>
